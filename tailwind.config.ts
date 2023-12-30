7c22e6a11fb812e7d62a14dd694ee8a39ba11643
Calisthetic <vovakalash04@mail.ru>
2023-12-30 21:27:15 +0500

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,32 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {        
+        primary: "var(--color-primary)",
+        primaryHover: "var(--color-primary-hover)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        neutral: "var(--color-neutral)",
+        borders: "var(--color-borders)",
+        text: "var(--color-font)",
+        buttons: "var(--color-buttons)",
+        buttonsHover: "var(--color-buttons-hover)",
+        icons: "var(--color-icons)",
+        shadows: "var(--color-shadows)",
+        info: "var(--color-info)",
+        success: "var(--color-success)",
+        warning: "var(--color-warning)",
+        error: "var(--color-error)",
+        roundedBtn: "var(--value-rounded-btn)",
+        roundedBox: "var(--value-rounded-box)",
+        borderWidth: "var(--value-border-width)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({addVariant}:any) {
+      addVariant('child', '&>*');
+      addVariant('child-hover', '&>*:hover');
+    }],
 }
 export default config
