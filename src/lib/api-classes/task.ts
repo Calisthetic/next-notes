c8ff7f2d4b937afdebce54cd2ac556c3579b1c6f
Calisthetic <vovakalash04@mail.ru>
2024-01-06 22:45:53 +0500

    api

--- src/lib/api-classes/task.ts
+++ src/lib/api-classes/task.ts
@@ -1,14 +1,16 @@
 import { BaseModel } from "./base-model";
 
 export class Task extends BaseModel {
-  text: string
+  title: string
   time: Date
   userId: string
+  text: string
 
   constructor(arr:any[]) {
     super(arr[0])
-    this.text = arr[1]
+    this.title = arr[1]
     this.time = arr[2]
     this.userId = arr[3]
+    this.text = arr[4]
   }
 }
--- src/pages/api/products-of-users/index.ts
+++ src/pages/api/products-of-users/index.ts
@@ -1,3 +1,4 @@
+import { Product } from '@/src/lib/api-classes/product';
 import { ProductOfUser } from '@/src/lib/api-classes/products-of-user';
 import getSheetClient from '@/src/lib/sheet-client';
 import { getSheetLetter } from '@/src/lib/sheet-letters';
@@ -13,12 +14,41 @@ export default async function handler(
       return res.status(401).send({message: 'Unauthorized'})
     }
     const sheets = await getSheetClient();
-    const response = await sheets.spreadsheets.values.get({
+    let responseProductsOfUsers = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.GOOGLE_SHEET_ID,
       range: "ProductsOfUsers!A2:" + getSheetLetter(new ProductOfUser([]))
     })
+    const responseProducts = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "Products!A2:" + getSheetLetter(new Product([])),
+    })
+    const responseProductCategories = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "ProductCategories!A2:" + getSheetLetter(new Product([])),
+    })
+
+    if (responseProductsOfUsers.status !== 200) {
+      return res.status(responseProductsOfUsers.status).send({message: responseProductsOfUsers.statusText})
+    } else if (!responseProductsOfUsers.data.values) {
+      return res.status(404).send({message: 'No products found'})
+    } else if (!responseProducts.data.values) {
+      return res.status(404).send({message: 'No products found'})
+    } else if (!responseProductCategories.data.values) {
+      return res.status(404).send({message: 'No products found'})
+    }
+    
+    for (let i = 0; i < responseProductsOfUsers.data.values.length; i++) {
+      const productId = responseProductsOfUsers.data.values[i][2]
+      const productObjectId = responseProducts.data.values.findIndex(x => x[0] === productId)
+      if (productObjectId === -1) {
+        return res.status(500).send({message: 'Something went wrong'})
+      }
+      responseProductsOfUsers.data.values[i][2] = responseProducts.data.values[productObjectId][1]
+      const categotyId = responseProducts.data.values[productObjectId][2]
+      responseProductsOfUsers.data.values[i].push(responseProductCategories.data.values[categotyId - 1][1])
+    }
     return res.status(200).json({
-      data: response.data.values?.filter(x => x.length > 0 && x[1] === userId)
+      data: responseProductsOfUsers.data.values
     })
   } catch(e:any) {
     return res.status(500).send({message: e.message ?? 'Something went wrong'})
